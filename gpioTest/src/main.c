b2a80a746fc072e42cf9fcae2ce31b58d6a56526
augustkimo <72067396+augustkimo@users.noreply.github.com>
2021-07-28 05:23:19 -0400

    Update main.c

--- gpioTest/src/main.c
+++ gpioTest/src/main.c
@@ -25,18 +25,4 @@ void main(void)
 		led_is_on = !led_is_on;
 		k_msleep(SLEEP_TIME_MS);
 	}
-  // struct device *dev;
-  // dev = device_get_binding("gpio0");
-  //
-  // gpio_pin_configure(dev, 29, GPIO_OUTPUT_ACTIVE | FLAGS);
-  // gpio_pin_configure(dev, 30, GPIO_OUTPUT_ACTIVE | FLAGS);
-  //
-  // while(true){
-  //   k_sleep(K_SECONDS(1.5));
-  //   gpio_pin_set(dev, 29, 0);
-  //   gpio_pin_set(dev, 30, 0);
-  //   k_sleep(K_SECONDS(1.5));
-  //   gpio_pin_set(dev, 29, 1);
-  //   gpio_pin_set(dev, 30, 1);
-  // }
 }
